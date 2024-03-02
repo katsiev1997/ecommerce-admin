1322070e3339469349b3549e05b2fb9899b4cd32
katsiev1997 <katsiev1997@gmail.com>
2024-03-02 22:49:10 +0300

    Modal components (Admin)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
+import { ModalProvider } from "@/providers/modal-provider";
+import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,9 +19,11 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <ModalProvider />
+          {children}
+        </body>
       </html>
     </ClerkProvider>
-    
   );
 }
